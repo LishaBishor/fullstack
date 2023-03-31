396b40ef2e886c18f441147ab14edb485617d630
LishaBishor <129526801+LishaBishor@users.noreply.github.com>
2023-03-31 20:29:01 +0530

    Initial commit

---
+++ basics1.js
@@ -0,0 +1,56 @@
+// a=5;
+// let b=10
+// console.log(b)
+// const pi=3.14
+// console.log(pi)
+// a=["lin",10,]
+// console.log(a[3])
+// let person= {
+//         Name:"Lila",
+//         age:56
+//             }
+// console.log(person.Name)
+// a=4;
+// b='3';
+// if(a==b)
+// {console.log("yes")}
+// function mul(a,b)
+// {var c=a*b;
+// console.log(c);}
+// mul(4,5);
+// function div(a,b)
+// {var c=a/b;
+// console.log(c);}
+// div(4,5);
+// let student={
+//     name : "selesh",
+//     age : 23,
+//     degree:"Btech"
+// }
+// console.log(student)
+// let months=["january","feb",8,7,"march"]
+// console.log(months[3])
+// function welcome(){
+//     console.log("Welcome to my first function")
+// }
+// welcome
+console.log("Hello world")
+// let sum =0;
+// for(let i=1;i<=10;i++)
+// {sum=sum+i;
+//     }
+//     console.log('sum of first 10 natuaral numbers',sum)
+// let i=0;
+// while(i<=10)
+// {console.log(i);
+// i++;}
+// let i=2;
+// do{
+//     console.log(i);
+// }while(i>2)
+
+let students=['hari','rema','lish']
+for (let stud of students){
+    console.log(`hello ${stud}`)
+}
+//backtick notation
