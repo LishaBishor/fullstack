ab9a2b168cef1a65de7a7989c94a4ea00e6799e6
LishaBishor <129526801+LishaBishor@users.noreply.github.com>
2023-04-03 21:08:57 +0530

    nnnn

--- basics1.js
+++ basics1.js
@@ -49,8 +49,49 @@ console.log("Hello world")
 //     console.log(i);
 // }while(i>2)
 
-let students=['hari','rema','lish']
-for (let stud of students){
-    console.log(`hello ${stud}`)
-}
+// let students=['hari','rema','lish']
+// for (let stud of students){
+//     console.log(`hello ${stud}`)
+// }
 //backtick notation
+// function findDigits(n) {
+   
+//     var number=n
+//     count=0
+//     while(n>0)
+//         { var digit=n%10
+//          if(number%digit==0)
+//          {count=count+1}
+//         n=Math.floor(n/10)
+//         console.log(n)
+//         console.log(digit)}
+//         return count
+// }
+// result=findDigits(360)
+// console.log("number of digits"+count)
+function timeConversion(s) {
+    // Write your code here
+    if(s.substring(8)=='PM'&& s.substring(0,2)=='12')
+    {return(s.substring(0,8))}
+    if(s.substring(8)=='PM'&& s.substring(0,2)!='12')
+    {
+       
+        p=12+Number(s.substring(0,2))
+        k=String(p).concat( s.substring(2,8))
+        return k
+        
+    
+    }
+    if(s.substring(8)=='AM'&& s.substring(0,2)=='12')
+    { k="00".concat( s.substring(2,8))
+        return k}
+    if(s.substring(8)=='AM'&& s.substring(0,2)!='12')
+     {return(s.substring(0,8))}
+}
+
+    
+
+
+ var h
+ h=timeConversion("02:01:02AM")
+ console.log(h)
